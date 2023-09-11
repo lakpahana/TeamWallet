e96d9bf6b1bb6eecdccc74a07a2ce65df36cae99
AGS Lakpahana <agslakpahana@gmail.com>
2023-09-11 20:36:14 +0530

    keys

--- backend/service/firebaseService.js
+++ backend/service/firebaseService.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth,RecaptchaVerifier } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC6mlhag6h3lgCbfmCdVgJwTwUhyqsTRN8",
-    authDomain: "teamwallet700.firebaseapp.com",
-    projectId: "teamwallet700",
-    storageBucket: "teamwallet700.appspot.com",
-    messagingSenderId: "203401155043",
-    appId: "1:203401155043:web:50fbe42d24462750c810d6",
-    measurementId: "G-DEK0ELVGHE"
+    // apiKey: "AIzaSyC6mlhag6h3lgCbfmCdVgJwTwUhyqsTRN8",
+    // authDomain: "teamwallet700.firebaseapp.com",
+    // projectId: "teamwallet700",
+    // storageBucket: "teamwallet700.appspot.com",
+    // messagingSenderId: "203401155043",
+    // appId: "1:203401155043:web:50fbe42d24462750c810d6",
+    // measurementId: "G-DEK0ELVGHE"
 };
 
 const app = initializeApp(firebaseConfig);
