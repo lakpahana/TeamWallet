4ada4425ddcd6084266f3d08d608c8de58876b14
AGS Lakpahana <agslakpahana@gmail.com>
2023-09-17 23:51:38 +0530

    expense

---
+++ backend/controllers/expense.js
@@ -0,0 +1,46 @@
+//controler for expense
+const Expense = require("../models/expense");
+const User = require("../models/user");
+const { errorHandler } = require("../helpers/dbErrorHandler");
+
+exports.expenseById = (req, res, next, id) => {
+    Expense.findById(id).exec((err, expense) => {
+        if (err || !expense) {
+        return res.status(400).json({
+            error: "Expense not found",
+        });
+        }
+        req.expense = expense;
+        next();
+    });
+    }
+
+exports.create = (req, res) => {
+    const expense = new Expense(req.body);
+    expense.save((err, data) => {
+        if (err) {
+        return res.status(400).json({
+            error: errorHandler(err),
+        });
+        }
+        res.json({ data });
+    });
+}
+
+exports.read = (req, res) => {
+    return res.json(req.expense);
+}
+
+exports.update = (req, res) => {
+    const expense = req.expense;
+    expense.name = req.body.name;
+    expense.amount = req.body.amount;
+    expense.save((err, data) => {
+        if (err) {
+        return res.status(400).json({
+            error: errorHandler(err),
+        });
+        }
+        res.json(data);
+    });
+}
