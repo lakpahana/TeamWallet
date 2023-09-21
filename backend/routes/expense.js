0cb736f4b0ba77d9152f6ce5c7f0f9643035fb1b
AGS Lakpahana <agslakpahana@gmail.com>
2023-09-21 23:59:59 +0530

    test route

--- backend/routes/expense.js
+++ backend/routes/expense.js
@@ -1,2 +1,21 @@
+const express = require('express');
+const router = express.Router();
 
+// Load Expense model
+const Expense = require('../models/Expense');
+
+// @route GET api/expense/test
+// @description tests expense route
+// @access Public
+router.get('/test', (req, res) => res.send('expense route testing!'));
+
+// @route GET api/expense
+// @description Get all expense
+// @access Public
+router.get('/', (req, res) => {
+    Expense.find()
+        .then(expenses => res.json(expenses))
+        .catch(err => res.status(404).json({ noexpensesfound: 'No Expenses found' }));
+    }
+);
 
